3c41e4c8a4715039437bfd98d01f40b561199f04
kyosuke uryu <kyo.uryu33@gmail.com>
2020-01-01 23:50:49 +0900

    [add]deviseによる新規登録、確認メール、ログイン実装

--- app/controllers/unread_books_controller.rb
+++ app/controllers/unread_books_controller.rb
@@ -1,5 +1,7 @@
 class UnreadBooksController < ApplicationController
   before_action :set_book, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: :tops
+
   def tops; end
 
   def index
