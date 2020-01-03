bef943978bed55b51eeda19e1b7fba2dc6ddab28
kyosuke uryu <kyo.uryu33@gmail.com>
2020-01-03 22:06:08 +0900

    [update]let利用による共通化

--- spec/system/unread_books.rb
+++ spec/system/unread_books.rb
@@ -2,18 +2,19 @@ require 'rails_helper'
 
 describe '未読書籍の管理機能', type: :system do
   describe '未読書籍一覧表示機能' do
+    let(:user_a) { FactoryBot.create(:user, email: 'a@example.com') }
+    let(:user_b) { FactoryBot.create(:user, email: 'b@example.com') }
+
     before do
-      user_a = FactoryBot.create(:user, email: 'a@example.com')
       FactoryBot.create(:unread_book, title: '最初の本', user: user_a)
+      visit new_user_session_path
+      fill_in 'メールアドレス', with: login_user.email
+      fill_in 'パスワード', with: login_user.password
+      click_button 'Log in'
     end
 
     context 'ユーザーAがログインしている時' do
-      before do
-        visit new_user_session_path
-        fill_in 'メールアドレス', with: 'a@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'Log in'
-      end
+      let(:login_user) { user_a }
 
       it 'ユーザーAが登録した書籍が表示される' do
         visit unread_books_path
@@ -22,13 +23,7 @@ describe '未読書籍の管理機能', type: :system do
     end
 
     context 'ユーザーBがログインしている時' do
-      FactoryBot.create(:user, email: 'b@example.com')
-      before do
-        visit new_user_session_path
-        fill_in 'メールアドレス', with: 'b@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'Log in'
-      end
+      let(:login_user) { user_b }
 
       it 'ユーザーAが登録した書籍が表示されない' do
         visit unread_books_path
