4a14e1bed0ff330eac5636d74e5b3edfc3fd6af7
kyosuke uryu <kyo.uryu33@gmail.com>
2020-01-04 17:25:52 +0900

    [add]既読書籍登録時に未読テーブルから削除されるように実装

--- app/controllers/finished_books_controller.rb
+++ app/controllers/finished_books_controller.rb
@@ -4,12 +4,26 @@ class FinishedBooksController < ApplicationController
   end
 
   def new
-    @finished_book = FinishedBook.new
+    @unread_book = current_user.unread_books.find(params[:id])
+    @finished_book = current_user.finished_books.new
     @finished_book.title = params[:title]
     @finished_book.author = params[:author]
   end
 
   def create
+    @unread_book = current_user.unread_books.find(params[:finished_book][:unread_book][:id])
+    @finished_book = current_user.finished_books.new(book_params)
+    if @finished_book.save
+      @unread_book.destroy
+      redirect_to root_path, notice: '書籍読破おめでとうございます！'
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def book_params
+    params.require(:finished_book).permit(:title, :author, :status, :desposal_expired, :impression, :unread_book)
   end
 end
