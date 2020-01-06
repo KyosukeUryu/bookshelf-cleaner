d6122205d91d83de60d773e2430f7da6f88d9a39
kyosuke uryu <kyo.uryu33@gmail.com>
2020-01-06 12:24:55 +0900

    [add]conversationモデルのアソシエーション等記載

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,2 +1,10 @@
 class Conversation < ApplicationRecord
+  belongs_to :sender, foreign_key: :sender_id, class: 'User'
+  belongs_to :recipient, foreign_key: :recipient_id, class: 'User'
+
+  validates_uniquness_of :sender_id, scope: :recipient_id
+
+  scope :between, -> (sender_id, recipient_id) do
+    where(["(conversation.sender_id = ? AND conversation.recipient_id = ?) OR (conversation.sender_id = ? AND conversation.recipient_id = ?)", sender_id, recipient_id, recipient_id, sender_id])
+  end
 end
