59c1cb81471d2682b4a733f8b3a30d9fbd5313f7
kyosuke uryu <kyo.uryu33@gmail.com>
2020-01-07 20:07:37 +0900

    [add]気になる書籍から解除する機能を実装

--- app/controllers/concern_books_controller.rb
+++ app/controllers/concern_books_controller.rb
@@ -1,11 +1,12 @@
 class ConcernBooksController < ApplicationController
 
   def create
-    @concern_book = current_user.concern_books.create(finished_book_id: params[:finished_book_id])
-    redirect_to others_books_finished_books_path, notice: 'お気に入りに登録しました'
+    current_user.concern_books.create(finished_book_id: params[:finished_book_id])
+    redirect_to others_books_finished_books_path, notice: '気になる書籍に追加しました'
   end
 
   def destroy
-
+    current_user.concern_books.find(params[:id]).destroy
+    redirect_to others_books_finished_books_path, notice: '気になる書籍から外しました'
   end
 end
