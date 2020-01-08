680f0c9f5fdd029199636fae1d6ca8660453193d
kyosuke uryu <kyo.uryu33@gmail.com>
2020-01-08 11:35:43 +0900

    [add]読書中書籍の進捗情報の作成、削除機能を実装

--- app/controllers/progresses_controller.rb
+++ app/controllers/progresses_controller.rb
@@ -1,9 +1,22 @@
 class ProgressesController < ApplicationController
   def create
-
+    progress = current_user.progresses.new(progress_params)
+    if progress.save
+      redirect_to reading_books_unread_books_path, notice: '進捗メモを更新しました'
+    else
+      redirect_to reading_books_unread_books_path, notice: 'メモを更新できませんでした'
+    end
   end
 
   def destroy
+    progress = Progress.find(params[:id])
+    progress.destroy
+    redirect_to reading_books_unread_books_path, notice: 'メモを削除しました'
+  end
 
+  private
+
+  def progress_params
+    params.require(:progress).permit(:content)
   end
 end
