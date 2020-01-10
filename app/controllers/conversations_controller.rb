bac47beb44e459d858f9229ef1026b2305d6b21d
kyosuke uryu <kyo.uryu33@gmail.com>
2020-01-10 12:45:13 +0900

    [add]Timezone設定

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -7,6 +7,7 @@ class ConversationsController < ApplicationController
   def create
     if Conversation.between(params[:sender_id], params[:recipient_id]).present?
       @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
+      redirect_to conversation_messages_path(@conversation)
     else
       @conversation = Conversation.create!(conversation_params)
       redirect_to conversation_messages_path(@conversation)
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module BookManageApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
+    config.time_zone = 'Asia/Tokyo'
 
     config.generators do |g|
       g.test_framework false
