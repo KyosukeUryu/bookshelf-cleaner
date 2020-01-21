41b73b89b7c404c377ebc3746541743b7a6d586c
kyosuke uryu <kyo.uryu33@gmail.com>
2020-01-21 12:48:10 +0900

    [add]deviseのフラッシュメッセージ等の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   root to: 'statics#top'
 
   resources :unread_books do
