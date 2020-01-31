161d4f54766c72eb8676f860bdcfeb7b284651bb
kyosuke uryu <kyo.uryu33@gmail.com>
2020-01-31 12:57:37 +0900

    [add]seedデータ投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,45 @@
-30.times do |i|
+User.create!(
+  name: "test_admin",
+  email: "test@admin.com",
+  password: "password",
+  admin: true,
+  confirmed_at: DateTime.now
+)
+10.times do |i|
   UnreadBook.create!(
     title: "hoge#{i}",
     author: "test#{i}",
     status: 0,
-    reading_expired: Time.now,
+    user_id: User.last.id
+  )
+end
+10.times do |i|
+  FinishedBook.create!(
+    title: "fuga#{i}",
+    author: "example#{i}",
+    status: 0,
+    user_id: User.last.id
+  )
+end
+User.create!(
+  name: "other_user",
+  email: "other@test.com",
+  password: "password",
+  confirmed_at: DateTime.now
+)
+10.times do |i|
+  UnreadBook.create!(
+    title: "other_hoge#{i}",
+    author: "other_test#{i}",
+    status: 0,
+    user_id: User.last.id
+  )
+end
+10.times do |i|
+  FinishedBook.create!(
+    title: "other_fuga#{i}",
+    author: "other_example#{i}",
+    status: 0,
     user_id: User.last.id
   )
 end
