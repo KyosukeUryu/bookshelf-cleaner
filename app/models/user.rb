09e75cb7f0ba68816593e3d246c7510115ee9c7a
kyosuke uryu <kyo.uryu33@gmail.com>
2020-02-03 11:11:38 +0900

    [update]userモデルバリデーション変更、specファイルの名称変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,9 +44,7 @@ class User < ApplicationRecord
   has_many :progresses, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 40 }
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :email, length: { maximum: 255 }
   mount_uploader :avater, ImageUploader
 
   def concerned?(book)
