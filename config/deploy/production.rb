e74df6b1fb7c69fa43e07c4a7cb354e5215f5736
kyosuke uryu <kyo.uryu33@gmail.com>
2020-02-18 17:15:20 +0900

    [add]deploy設定変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,13 @@
-server '3.114.215.170', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '/Users/lalalabaybay/.ssh/id_rsa'
+# server '3.114.215.170', user: 'app', roles: %w{app db web}
+# set :ssh_options, keys: '/Users/lalalabaybay/.ssh/id_rsa'
+server '3.114.215.170',
+   user: "app",
+   roles: %w{web db app},
+   ssh_options: {
+       port: 22022,
+       user: "app", # overrides user setting above
+       keys: %w(~/.ssh/id_rsa),
+       forward_agent: true
+#     auth_methods: %w(publickey password)
+#     # password: "please use keys"
+   }
