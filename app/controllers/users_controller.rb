1ad435d27f07be283a839d68da92bb7fcecc396d
kyosuke uryu <kyo.uryu33@gmail.com>
2020-02-23 23:30:43 +0900

    [update]tagの名前が同一の時他ユーザーのタグが反映されるバグを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,17 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   def show
     @user = User.find(params[:id])
-    @hash = {}
+    if @user.has_tags?
+      @hash = {}
+      @user.finished_books.each do |book|
+        book.tag_list.each do |tag|
+          if @hash[tag].present?
+            @hash[tag] += 1
+          else
+            @hash[tag] = 1
+          end
+        end
+      end
+    end
   end
 end
