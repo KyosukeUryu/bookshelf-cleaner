befc9761eba10867beb24be8241d3d8e3ba85034
kyosuke uryu <kyo.uryu33@gmail.com>
2020-02-28 10:29:17 +0900

    [add]コメント追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
       @hash = {}
       @user.finished_books.each do |book|
         book.tag_list.each do |tag|
+          # @hash[tag]があれば値を増加、なければ1で設定
           if @hash[tag].present?
             @hash[tag] += 1
           else
